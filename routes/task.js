a4cf272fde4cbfa0055603bda29c9670375a6951
iftekher ahmed <139613310+iftekher6@users.noreply.github.com>
2024-07-07 18:17:06 +0600

    nodeenv

--- routes/task.js
+++ routes/task.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { deleteTask, getMyTask, newTask, updateTask } from '../contorllers/task.js';
-// import { isAuthenticated } from '../middlewars/auth.js';
+import { isAuthenticated } from '../middlewars/auth.js';
 
 const router = express.Router();
 
-router.post('/new', newTask);
-router.get('/my',  getMyTask);
-router.route('/:id').put( updateTask).delete(deleteTask);
+router.post('/new', isAuthenticated, newTask);
+router.get('/my', isAuthenticated, getMyTask);
+router.route('/:id').put(isAuthenticated, updateTask).delete(isAuthenticated, deleteTask);
 
 export default router;
 
